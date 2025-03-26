63d0bb3592059456077a0a7429c9645bbfb08716
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-03-26 12:23:39 +0700

    membuat variable data temporer

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -33,6 +33,23 @@ for  (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data 
 }
 
 
+for (i = 1; i <= n - 1; i++)
+{ // Step 1
+
+temp = arr[i]; // Step 2
+
+j = i - 1; // Step 3
+
+while (j >= 0 && arr[j] > temp) // Step 4
+{
+    arr[j + 1] = arr[j]; // Step 4a
+    j--; // Step 4b
+}
+
+arr [j + 1] = temp; // Step 5
+}
+}
+
 void display()
 {
     cout << endl; // Output baris kosong 
