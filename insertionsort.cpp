03a8ea21ca31add3a1ffa74beab6512270e2ea33
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-03-26 12:30:26 +0700

    output baris kosong

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -11,7 +11,19 @@ void input()
         cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
         cin >> n; // memanggil variable inputan n
     
-
+        if (n <= 20)
+        { // Mmembuat Kondisi n tidak lebih dari 20
+        break;
+        }
+        else 
+        {
+            cout << "\nArray yang anda masukkan maksimal 2o elemen.\n"; // Menampilkan pesan jika data lebih dari 20
+        }
+        }
+        cout << endl; // Membuat jarak per baris program
+        cout << "===============" << endl; // Membuat tampilan susunan data elemen array
+        cout << "Masukkan Element Array" << endl;
+        cout << "==============" << endl;
 
 for  (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
 {
@@ -43,21 +55,7 @@ arr [j + 1] = temp; // Step 5
 }
 }
 
-void display()
-{
-    cout << endl; // Output baris kosong 
-    cout << "====================" << endl; 
-    cout << "Total Pass = " << n - 1 << endl; // count element movement
-    cout << "===================" << endl; // Output ke layar
-    cout << "Element Array yang telah tersusun" << endl; // Output ke layar
-    cout << "===================" << endl; // Output ke layar
 
-for (int j = 0; j < n; j++)
-{ // Looping dengan j dimulai dari 0 hingga n-1
-cout << arr[j] << endl; // Output ke laayr
-}
-cout << endl; // Output baris kosong
-}
 
 int main()
 {
