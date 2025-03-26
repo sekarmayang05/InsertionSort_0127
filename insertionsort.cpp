aa1a5988e90812c301f24a057dc756f02e1685a2
sekarmayang05 <sekarmayangkumaladewi@gmail.com>
2025-03-26 12:27:38 +0700

    membuat nilai n tidak lebih dari 20

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -11,19 +11,7 @@ void input()
         cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
         cin >> n; // memanggil variable inputan n
     
-if (n <= 20)
-{ // Mmembuat Kondisi n tidak lebih dari 20
-break;
-}
-else 
-{
-    cout << "\nArray yang anda masukkan maksimal 2o elemen.\n"; // Menampilkan pesan jika data lebih dari 20
-}
-}
-cout << endl; // Membuat jarak per baris program
-cout << "===============" << endl; // Membuat tampilan susunan data elemen array
-cout << "Masukkan Element Array" << endl;
-cout << "==============" << endl;
+
 
 for  (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data pada array
 {
@@ -32,6 +20,11 @@ for  (int i = 0; i < n; i++) // Menggunakan perulangan for untuk menyimpan data 
 }
 }
 
+void insertionSort()
+{ // Procedure Insertionsort
+
+int temp; // Membuat variable data temporer atau penyimpanan sementara
+int j, i; // Membuat variable j sebagai penanda
 
 for (i = 1; i <= n - 1; i++)
 { // Step 1
